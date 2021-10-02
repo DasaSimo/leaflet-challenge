85e7a400a57b4dc48d1e2c4c390109c104512076
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-10-02 00:36:41 -0400

    changing map sets in step 2, adding map tiles to step 2

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,7 @@
+// Colors of markers
+var myColors = ['#ecffb3', '#99cc00', '#ffcc00', ' #ff9900', '#cc0000', '#33001a'];
+
 // Store the API endpoint as queryUrl.
-
-var myColors=['#ecffb3','#99cc00','#ffcc00',' #ff9900','#cc0000','#33001a'];
-
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
@@ -11,44 +11,43 @@ d3.json(queryUrl).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-
   function markerSize(mag) {
-    return (mag) * 6;
+    return (mag) * 5;
   }
 
   function marker(feature, latlng) {
     var col;
     var colFill;
-    var dep=feature.geometry.coordinates[2];
-    if (dep<10) {
-      colFill=myColors[0]
+    var dep = feature.geometry.coordinates[2];
+    if (dep < 10) {
+      colFill = myColors[0]
     }
-    else if (dep<30) {
-     colFill=myColors[1]
+    else if (dep < 30) {
+      colFill = myColors[1]
     }
-    else if (dep<50) {
-      colFill=myColors[2]
+    else if (dep < 50) {
+      colFill = myColors[2]
     }
-    else if (dep<70) {
-      colFill=myColors[3]
+    else if (dep < 70) {
+      colFill = myColors[3]
     }
-    else if (dep<90) {
-      colFill=myColors[4]
+    else if (dep < 90) {
+      colFill = myColors[4]
     }
     else {
-      colFill=myColors[5]
+      colFill = myColors[5]
     }
     return new L.CircleMarker(latlng, {
       fillOpacity: 0.75,
       color: '#555',
       weight: 1,
       fillColor: colFill,
-      
+
       radius: markerSize(feature.properties.mag)
-    }); 
+    });
   }
 
-  
+
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}<br>Magnitude: ${(feature.properties.mag)}
     </p>`);
@@ -58,34 +57,17 @@ function createFeatures(earthquakeData) {
   // Run the onEachFeature function once for each piece of data in the array.
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
-    pointToLayer : marker
+    pointToLayer: marker
   });
 
   // Send our earthquakes layer to the createMap function/
   createMap(earthquakes);
 }
 
-//  when using MapBox
-// Creating initial map object mapBox
-// var myMAp = L.map("map", {
-// center: [45.52, -122.67],
-// zoom: 13  
-// });
-
-// adding a tile layer (the background map image)
-// L.tileLayer("https://api.mapbax.com/style/v1/{id}/tiles/{z}/{x}/{y}?acces_token={accessToken}", {
-//   attribution : "&copy; <a href='http://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/',target='_blank>Improve this map</a></strong>,
-//   tileSize: 512,
-//   maxZoom : 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY  
-// }).addTo(myMap);
-
 
 function createMap(earthquakes) {
 
-  // Create the base layers.
+  //Create the base layers
   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
@@ -93,6 +75,7 @@ function createMap(earthquakes) {
   var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
+
 
   // Create a baseMaps object.
   var baseMaps = {
@@ -110,8 +93,8 @@ function createMap(earthquakes) {
     center: [
       37.09, -95.71
     ],
-    zoom: 5,
-    layers: [street, earthquakes]
+    zoom: 3,
+    layers: [topo, earthquakes]
   });
 
   // Create a layer control.
@@ -123,17 +106,17 @@ function createMap(earthquakes) {
 
   var legend = L.control({ position: "bottomright" });
 
-  legend.onAdd = function(map) {
+  legend.onAdd = function (map) {
     var div = L.DomUtil.create("div", "legend");
     div.innerHTML += "<b>Depth</b><br>";
-    div.innerHTML += '<i style="background: '+myColors[0]+'"></i><span>&lt;10</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[1]+'"></i><span>10-30</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[2]+'"></i><span>30-50</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[3]+'"></i><span>50-70</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[4]+'"></i><span>70-90</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[5]+'"></i><span>&gt;90</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[0] + '"></i><span>&lt;10</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[1] + '"></i><span>10-30</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[2] + '"></i><span>30-50</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[3] + '"></i><span>50-70</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[4] + '"></i><span>70-90</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[5] + '"></i><span>&gt;90</span><br>';
     return div;
   };
-  
-  legend.addTo(myMap);  
+
+  legend.addTo(myMap);
 }
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,60 +1,56 @@
+// Colors of markers
+var myColors = ['#ecffb3', '#99cc00', '#ffcc00', ' #ff9900', '#cc0000', '#33001a'];
+
 // Store the API endpoint as queryUrl.
-
-var myColors=['#ecffb3','#99cc00','#ffcc00',' #ff9900','#cc0000','#33001a'];
-
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
 var queryUrlPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
   d3.json(queryUrlPlates).then(function (plates) {
-
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features, plates.features);
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features, plates.features);
+  });
 });
-});
-
 
 function createFeatures(earthquakeData, platesData) {
 
-
   function markerSize(mag) {
-    return (mag) * 6;
+    return (mag) * 5;
   }
 
   function marker(feature, latlng) {
     var col;
     var colFill;
-    var dep=feature.geometry.coordinates[2];
-    if (dep<10) {
-     colFill=myColors[0]
+    var dep = feature.geometry.coordinates[2];
+    if (dep < 10) {
+      colFill = myColors[0]
     }
-    else if (dep<30) {
-     colFill=myColors[1]
+    else if (dep < 30) {
+      colFill = myColors[1]
     }
-    else if (dep<50) {
-     colFill=myColors[2]
+    else if (dep < 50) {
+      colFill = myColors[2]
     }
-    else if (dep<70) {
-     colFill=myColors[3]
+    else if (dep < 70) {
+      colFill = myColors[3]
     }
-    else if (dep<90) {
-     colFill=myColors[4]
+    else if (dep < 90) {
+      colFill = myColors[4]
     }
     else {
-     colFill=myColors[5]
+      colFill = myColors[5]
     }
     return new L.CircleMarker(latlng, {
       fillOpacity: 0.75,
       color: '#555',
       weight: 1,
       fillColor: colFill,
-      
+
       radius: markerSize(feature.properties.mag)
-    }); 
+    });
   }
 
-  
+
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}<br>Magnitude: ${(feature.properties.mag)}
     </p>`);
@@ -64,131 +60,126 @@ function createFeatures(earthquakeData, platesData) {
   // Run the onEachFeature function once for each piece of data in the array.
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
-    pointToLayer : marker
+    pointToLayer: marker
   });
 
   var plate = boundaries(platesData);
 
-  // Send our earthquakes layer to the createMap function/
+  // Send  earthquakes and plates  layer to the createMap function/
   createMap(earthquakes, plate);
 }
-
-//  when using MapBox
-// Creating initial map object mapBox
-// var myMAp = L.map("map", {
-// center: [45.52, -122.67],
-// zoom: 13  
-// });
-
-// adding a tile layer (the background map image)
-// L.tileLayer("https://api.mapbax.com/style/v1/{id}/tiles/{z}/{x}/{y}?acces_token={accessToken}", {
-//   attribution : "&copy; <a href='http://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/',target='_blank>Improve this map</a></strong>,
-//   tileSize: 512,
-//   maxZoom : 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY  
-// }).addTo(myMap);
 
 
 function createMap(earthquakes, plate) {
 
-  // Create the base layers.
-  //var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
- // })
+  // Create the base layers
 
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  var street_s = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/satellite-streets-v11",
+    accessToken: API_KEY
   });
 
-  var satelite =   L.tileLayer('https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=qFKAwVVTsPD48kA1clbr', {
-    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e"
-});
+  var mapBox_sat = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  });
 
-  var  outdoor = L.tileLayer('https://api.maptiler.com/tiles/outdoor/{z}/{x}/{y}.pbf?key=qFKAwVVTsPD48kA1clbr', {
-    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e"
-});
+  var mapBox_light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  });
+
+  var mapBox_out = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  });
 
   // Create a baseMaps object.
   var baseMaps = {
-    "Outdoor Map": outdoor,
-    "Topographic Map": topo,
-    "Satelite Map" : satelite
+    "Satellite": mapBox_sat,
+    "Light": mapBox_light,
+    "Outdoor": mapBox_out,
+    "Street - Satellite": street_s
   };
 
   // Create an overlay object to hold our overlay.
- 
-    
-
   var overlayMaps = {
-    Earthquakes: earthquakes, 
+    Earthquakes: earthquakes,
     "Tectonic Plates": plate,
-    "Greetings ": st(),
+    "Greetings ": sk(),
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
-    zoom: 5,
-    layers: [satelite, earthquakes]
+    zoom: 3,
+    layers: [mapBox_sat, earthquakes]
   });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
+  // Create a layer control
+  // Pass it our baseMaps and overlayMaps
+  // Add the layer control to the map
+
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
+  // legend
   var legend = L.control({ position: "bottomright" });
 
-  legend.onAdd = function(map) {
+  legend.onAdd = function (map) {
     var div = L.DomUtil.create("div", "legend");
     div.innerHTML += "<b>Depth</b><br>";
-    div.innerHTML += '<i style="background: '+myColors[0]+'"></i><span>&lt;10</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[1]+'"></i><span>10-30</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[2]+'"></i><span>30-50</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[3]+'"></i><span>50-70</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[4]+'"></i><span>70-90</span><br>';
-    div.innerHTML += '<i style="background: '+myColors[5]+'"></i><span>&gt;90</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[0] + '"></i><span>&lt;10</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[1] + '"></i><span>10-30</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[2] + '"></i><span>30-50</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[3] + '"></i><span>50-70</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[4] + '"></i><span>70-90</span><br>';
+    div.innerHTML += '<i style="background: ' + myColors[5] + '"></i><span>&gt;90</span><br>';
     return div;
   };
-  
-  legend.addTo(myMap);  
+
+  legend.addTo(myMap);
 }
 
-  function st() {
-    one=[];
-      var del=[[-1,2],[-2,0],[-1,-2],[0,-2],[4,-3],[4,3],[0,2],[-1,2],[-2,0],[-1,-2]];
-      x=19;y=52;
-      var os=[];
-      for(i=0;i<del.length;i++) {
-        os.push([y+=del[i][1],x+=del[i][0]])
-      }
-      one.push(L.polygon(os, {color: 'red'}));
-      return L.layerGroup(one); 
-  };
+// greetings
+function sk() {
+  var del = [[-1, 2], [-2, 0], [-1, -2], [0, -2], [4, -3], [4, 3], [0, 2], [-1, 2], [-2, 0], [-1, -2]];
+  x = 19; y = 50;
+  var os = [];
+  for (i = 0; i < del.length; i++) {
+    os.push([y += del[i][1], x += del[i][0]])
+  }
+  return L.layerGroup([L.polygon(os, { color: 'red' })]);
+};
 
-    
-    function boundaries(platesData) {
-      allPlates = []
-      for (var i = 0; i < platesData.length; i++) {
-        var plate = platesData[i];
-        var coordFlip=[];
-        coordinates=plate.geometry.coordinates;
-        for(j=0;j<coordinates.length;j++) {
-          coordFlip.push([coordinates[j][1],coordinates[j][0]]);
-        }
-        allPlates.push(
-          L.polyline(coordFlip)
-        )}
-        return L.layerGroup(allPlates);  
-      };
-     
-
-
-  
-
+// creating plates boundaries
+function boundaries(platesData) {
+  allPlates = []
+  for (var i = 0; i < platesData.length; i++) {
+    var plate = platesData[i];
+    var coordFlip = [];
+    coordinates = plate.geometry.coordinates;
+    for (j = 0; j < coordinates.length; j++) {
+      coordFlip.push([coordinates[j][1], coordinates[j][0]]);
+    }
+    allPlates.push(
+      L.polyline(coordFlip)
+    )
+  }
+  return L.layerGroup(allPlates);
+};
